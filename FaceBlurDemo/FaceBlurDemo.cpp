743449c7f5740be202966da9a9f4d4a994c11f0e
alkevich <alkevich@bsvt-nt.by>
2019-05-24 17:21:10 +0300

    project files

--- FaceBlurDemo/FaceBlurDemo.cpp
+++ FaceBlurDemo/FaceBlurDemo.cpp
@@ -1,4 +1,37 @@
-//
-// Created by yury on 24.5.19.
-//
+#include <iostream>
+#include <sys/types.h>
+#include <sys/stat.h>
 
+
+bool ValidateArgs(int argc, char *argv[]){
+
+    if(argc != 2){
+        return false;
+    }
+
+    char* path = argv[1];
+    struct stat fileInfo;
+
+    if(stat(path, &fileInfo) != 0){
+        std::cerr << "Cannot access " << path << std::endl;
+        return false;
+    }
+    else if((fileInfo.st_mode & S_IFDIR) == 0){
+        std::cerr << path << " is not a directory" << std::endl;
+        return false;
+    }
+
+    return true;
+}
+
+void PrintHelp(){
+    std::cout << "Usage: FaceBlurDemo pathToImagesFolder" << std::endl;
+}
+
+int main(int argc, char *argv[]){
+
+    if(ValidateArgs(argc, argv) == false){
+        PrintHelp();
+        exit(1);
+    }
+}
